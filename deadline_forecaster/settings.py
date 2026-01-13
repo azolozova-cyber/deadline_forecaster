69a1e52bb5c1b68fefa27e0f7e51b551a71d6770
azolozova-cyber <azolozova@mail.ru>
2026-01-14 00:58:31 +0500

    Update settings.py

--- deadline_forecaster/settings.py
+++ deadline_forecaster/settings.py
@@ -124,3 +124,18 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# settings.py
+
+DEBUG = False
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    ...
+]
+
